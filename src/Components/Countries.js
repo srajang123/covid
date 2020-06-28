8806e7111255508e177a7f3906d9f5a504b244a3
SRAJAN GUPTA <srajan@ubuntu.ubuntu-domain>
2020-06-29 01:34:33 +0530

    Show List Of Countries after sorting names in ascending order

--- src/Components/Countries.js
+++ src/Components/Countries.js
@@ -3,16 +3,44 @@ import axios from 'axios';
 import '../css/Countries.css';
 
 const Countries=props=>{
-    const [list,List]=useState('');
+    const [list,List]=useState([]);
+    const dataSort=(a,b)=>{
+        const p=a.Country.toLowerCase();
+        const q=b.Country.toLowerCase();
+        if(p>q)
+            return 1;
+        else if(p<q)
+            return -1;
+        else 
+            return 0;
+    }
     useEffect(()=>{
-        axios.get('')
+        axios.get('https://api.covid19api.com/countries')
         .then(res=>{
-            console.log(res);
+            res.data.sort(dataSort);
+            List(res.data);
         })
         .catch(err=>console.log(err));
     })
     return(
-        <div className='countries'></div>
+        <div className='countries'>
+            <table className="countries-list">
+                <thead>
+                    <tr>
+                        <th>S.No</th>
+                        <th>Country</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {list.map((el,idx)=>(
+                        <tr>
+                            <td>{idx+1}</td>
+                            <td><a href={'/country/'+el.Slug} className='country-link'>{el.Country}</a></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
 export default Countries;
