e11afe1a1313ded85323b60bdd2431744a069ed0
SRAJAN GUPTA <srajan@ubuntu.ubuntu-domain>
2020-06-28 16:29:37 +0530

    Improved UI

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,7 @@ import React,{useEffect} from 'react';
 import '../css/Header.css';
 const Header=props=>{
     let c1,c2,c3,c4;
-    c1=c2=c3=c4='nav-item covid-nav-item';
+    c1=c2=c3=c4='nav-link covid-nav-link';
     switch(window.location.pathname)
     {
         case '/': c1+=' covid-active';
@@ -17,7 +17,7 @@ const Header=props=>{
     }
     return(
         <div className='header'>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light covid-nav">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-light covid-nav">
                 <a className="navbar-brand covid-nav-brand" href='/'>Covid-19 Tracker</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
