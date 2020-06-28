02ca60ef7acb759ed438a43995bdbc5735bda86d
SRAJAN GUPTA <srajan@ubuntu.ubuntu-domain>
2020-06-29 01:09:10 +0530

    Basic Ountries API request

--- src/Components/Countries.js
+++ src/Components/Countries.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
 import '../css/Countries.css';
 
 const Countries=props=>{
+    const [list,List]=useState('');
+    useEffect(()=>{
+        axios.get('')
+        .then(res=>{
+            console.log(res);
+        })
+        .catch(err=>console.log(err));
+    })
     return(
         <div className='countries'></div>
     );
