bdd088e5a9966f0d9616c17bafd618d9012568b2
SRAJAN GUPTA <srajan@ubuntu.ubuntu-domain>
2020-06-28 15:47:56 +0530

    Basic styling of Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,27 +3,27 @@ import '../css/Header.css';
 const Header=props=>{
     return(
         <div className='header'>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href='/'>Covid-19 Tracker</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="/">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/world">World</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/countries">Countries</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/about">About</a>
-                    </li>
-                </ul>       
-            </div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light covid-nav">
+                <a className="navbar-brand covid-nav-brand" href='/'>Covid-19 Tracker</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto covid-navbar-nav">
+                        <li className="nav-item covid-nav-item active">
+                            <a className="nav-link covid-nav-link" href="/">Home</a>
+                        </li>
+                        <li className="nav-item covid-nav-item">
+                            <a className="nav-link covid-nav-link" href="/world">World</a>
+                        </li>
+                        <li className="nav-item covid-nav-item">
+                            <a className="nav-link covid-nav-link" href="/countries">Countries</a>
+                        </li>
+                        <li className="nav-item covid-nav-item">
+                            <a className="nav-link covid-nav-link" href="/about">About</a>
+                        </li>
+                    </ul>       
+                </div>
             </nav>
         </div>
     )
