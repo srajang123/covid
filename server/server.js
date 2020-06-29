70a5e93ef8ffcd6e6c7b716075f3a5f56a502c49
SRAJAN GUPTA <srajan@ubuntu.ubuntu-domain>
2020-06-29 14:04:33 +0530

    Added deployment

--- server/server.js
+++ server/server.js
@@ -1,28 +1,7 @@
 const express = require('express');
 const path = require('path');
-const nodemailer=require('nodemailer');
-const app = express();var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'youremail@gmail.com',
-      pass: 'yourpassword'
-    }
-  });
-  
-  var mailOptions = {
-    from: 'srajan.1721cs1138@kiet.edu',
-    to: 'srajan.oel@gmail.com',
-    subject: 'Visitor to Your Site',
-    text: 'There is a visitor to your site'
-  };
-  
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
+const app = express();
+
 app.use(express.static(path.join(__dirname, '..','build')));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, '..','build','index.html'));
